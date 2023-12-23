01da5d31a035de1ad276e2382430a64446f2b572
AmmoniumX <diego.garcia.cr@gmail.com>
2023-12-23 18:09:30 +0100

    Fluid machines save their data

--- src/main/java/com/ammonium/adminshop/blocks/entity/FluidBuyerBE.java
+++ src/main/java/com/ammonium/adminshop/blocks/entity/FluidBuyerBE.java
@@ -60,6 +60,8 @@ public class FluidBuyerBE extends FluidHandlerBlockEntity implements BuyerMachin
 
     public void setOwnerUUID(String ownerUUID) {
         this.ownerUUID = ownerUUID;
+        this.setChanged();
+        this.sendUpdates();
     }
 
     public String getOwnerUUID() {
@@ -68,6 +70,8 @@ public class FluidBuyerBE extends FluidHandlerBlockEntity implements BuyerMachin
 
     public void setAccount(Pair<String, Integer> account) {
         this.account = account;
+        this.setChanged();
+        this.sendUpdates();
     }
 
     public Pair<String, Integer> getAccount() {
@@ -75,6 +79,8 @@ public class FluidBuyerBE extends FluidHandlerBlockEntity implements BuyerMachin
     }
     public void setTargetShopItem(ShopItem item) {
         this.targetShopItem = item;
+        this.setChanged();
+        this.sendUpdates();
     }
 
     public ShopItem getTargetShopItem() {
