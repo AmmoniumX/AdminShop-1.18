6f9d352e5ee6467160cd473119b6ac9eb61e31ca
Diego Garcia <diego.garcia.cr@gmail.com>
2025-01-13 13:29:17 -0600

    Search through online players before API request in MojangAPI

--- src/main/java/com/ammonium/adminshop/network/MojangAPI.java
+++ src/main/java/com/ammonium/adminshop/network/MojangAPI.java
@@ -3,27 +3,64 @@ package com.ammonium.adminshop.network;
 import com.ammonium.adminshop.AdminShop;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MojangAPI {
-
     private static final Map<String, String> storedResults = new HashMap<>();
+
+    private static void storeInCache(String uuid, String name) {
+        AdminShop.LOGGER.debug("Storing {} to {} in cache.", uuid, name);
+        storedResults.put(uuid, name);
+    }
+
     public static String getUsernameByUUID(String uuid) {
-        // Search in stored results
+        // Search in cached results
         if (storedResults.containsKey(uuid)) {
-            AdminShop.LOGGER.debug("Retrieving "+uuid+" to "+storedResults.get(uuid)+" from cache.");
+            AdminShop.LOGGER.debug("Retrieving {} to {} from cache.", uuid, storedResults.get(uuid));
             return storedResults.get(uuid);
         }
+
+        // Search in online players
+        IntegratedServer server = Minecraft.getInstance().getSingleplayerServer();
+        if (server == null) {
+            AdminShop.LOGGER.error("Server is null, cannot get player list.");
+        } else {
+            PlayerList playerList = server.getPlayerList();
+            for (ServerPlayer player : playerList.getPlayers()) {
+                String playerUUID = player.getUUID().toString();
+                String playerName = player.getName().getString();
+                if (playerUUID.equals(uuid)) {
+                    storeInCache(uuid, playerName);
+                    return playerName;
+                }
+            }
+        }
+
         // Search in mojang API
-        AdminShop.LOGGER.debug("Name for "+uuid+" not found, using Mojang API...");
+        AdminShop.LOGGER.debug("Name for {} not found, using Mojang API...", uuid);
+        String apiResult = makeMojangAPIRequest(uuid);
+        if (apiResult != null) {
+            storeInCache(uuid, apiResult);
+            return apiResult;
+        }
+
+        // If all else fails, return the UUID
+        AdminShop.LOGGER.info("No name found, returning UUID");
+        return uuid;
+    }
+
+    private static String makeMojangAPIRequest(String uuid) {
         try {
             URL url = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -44,23 +81,21 @@ public class MojangAPI {
                 // Extract the username from the response JSON
                 String jsonResponse = response.toString();
                 JsonObject jsonObj = JsonParser.parseString(jsonResponse).getAsJsonObject();
-                // Extract the "name" field
-                String name = jsonObj.get("name").getAsString();
-
-                AdminShop.LOGGER.debug("Storing "+uuid+" to "+name+" in cache.");
-                storedResults.put(uuid, name);
-                return name;
+                if (jsonObj.has("name")) {
+                    return jsonObj.get("name").getAsString();
+                } else {
+                    AdminShop.LOGGER.warn("Mojang API response does not contain 'name' field: {}", jsonResponse);
+                }
             } else {
-                AdminShop.LOGGER.warn("Mojang API request failed: " + responseCode);
+                AdminShop.LOGGER.warn("Mojang API request failed: {}", responseCode);
             }
         }
         catch (IOException e) {
-            AdminShop.LOGGER.warn("Network exception while getting username. Using UUID for the remainder of the session.");
-            storedResults.put(uuid, uuid);
+            AdminShop.LOGGER.warn("Network exception while getting username. Will use UUID for the remainder of the session!");
+            return uuid;
         } catch (Exception e) {
-            AdminShop.LOGGER.error("Unhandled exception while getting username: " + e);
+            AdminShop.LOGGER.error("Unhandled exception while getting username: {}", e.toString());
         }
-        AdminShop.LOGGER.info("No name found, returning UUID");
-        return uuid;
+        return null;
     }
 }
